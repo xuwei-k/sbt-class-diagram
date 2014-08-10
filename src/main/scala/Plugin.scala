d8cf22471bee240e517041b3297cf3f32769a227
xuwei-k <6b656e6a69@gmail.com>
2014-08-10 10:51:33 +0900

    add classDiagramWrite

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -6,23 +6,30 @@ import sbt.complete.Parsers
 object Plugin extends sbt.Plugin {
 
   object DiagramKeys {
-    val classDiagram = InputKey[Unit]("classDiagram")
+    val classDiagram = InputKey[File]("classDiagram", "write svg file and open")
+    val classDiagramWrite = InputKey[File]("classDiagramWrite", "write svg file")
     val fileName = SettingKey[String]("classDiagramFileName")
   }
 
   import DiagramKeys._
 
+  private[this] val writeTask: Def.Initialize[InputTask[File]] = Def.inputTask{
+    val classes = Parsers.spaceDelimited("<class names>").parsed
+    val loader = (testLoader in Test).value
+    val clazz = loader.loadClass(classes.head)
+    val dot = Diagram(loader, classes.toList)
+    val svg = Keys.target.value / fileName.value
+    IO.writeLines(svg, dot :: Nil)
+    svg
+  }
+
   val classDiagramSettings: Seq[Def.Setting[_]] = Seq(
     fileName := "classDiagram.svg",
-    classDiagram := {
-      val classes = Parsers.spaceDelimited("<class names>").parsed
-      val loader = (testLoader in Test).value
-      val clazz = loader.loadClass(classes.head)
-      val dot = Diagram(loader, classes.toList)
-      val svg = Keys.target.value / fileName.value
-      IO.writeLines(svg, dot :: Nil)
+    classDiagram <<= writeTask.map{ svg =>
       java.awt.Desktop.getDesktop.open(svg)
-    }
+      svg
+    },
+    classDiagramWrite <<= writeTask
   )
 
 }
