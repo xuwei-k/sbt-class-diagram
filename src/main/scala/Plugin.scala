96bbb9e57f46db26b3fc7e8ecc805efb3449a624
xuwei-k <6b656e6a69@gmail.com>
2014-08-11 00:29:43 +0900

    parser

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -1,7 +1,10 @@
 package diagram
 
-import sbt._, Keys._
-import sbt.complete.Parsers
+import sbt.Keys._
+import sbt._
+import sbt.complete.DefaultParsers._
+import sbt.complete.Parser
+import xsbti.api.ClassLike
 
 object Plugin extends sbt.Plugin {
 
@@ -9,21 +12,37 @@ object Plugin extends sbt.Plugin {
     val classDiagram = InputKey[File]("classDiagram", "write svg file and open")
     val classDiagramWrite = InputKey[File]("classDiagramWrite", "write svg file")
     val fileName = SettingKey[String]("classDiagramFileName")
+    val classNames = TaskKey[Seq[String]]("classDiagramClassNames")
   }
 
-  import DiagramKeys._
+  import diagram.Plugin.DiagramKeys._
+  import sbinary.DefaultProtocol._
+  import sbt.Cache.seqFormat
 
-  private[this] val writeTask: Def.Initialize[InputTask[File]] = Def.inputTask{
-    val classes = Parsers.spaceDelimited("<class names>").parsed
-    val loader = (testLoader in Test).value
-    val clazz = loader.loadClass(classes.head)
-    val dot = Diagram(loader, classes.toList)
-    val svg = Keys.target.value / fileName.value
-    IO.writeLines(svg, dot :: Nil)
-    svg
-  }
+  private[this] val defaultParser: Parser[Seq[String]] =
+    (Space ~> token(StringBasic, "<class name>")).*
+
+  private[this] val writeTask: Def.Initialize[InputTask[File]] =
+    InputTask.createDyn(
+      Defaults.loadForParser(classNames)(
+       (state, classes) => classes.fold(defaultParser)(createParser)
+      )
+    ){
+      Def.task{ classes: Seq[String] =>
+        Def.task{
+          val loader = (testLoader in Test).value
+          val clazz = loader.loadClass(classes.head)
+          val dot = Diagram(loader, classes.toList)
+          val svg = Keys.target.value / fileName.value
+          IO.writeLines(svg, dot :: Nil)
+          svg
+        }
+      }
+    }
 
   val classDiagramSettings: Seq[Def.Setting[_]] = Seq(
+    classNames := Tests.allDefs((compile in Compile).value).collect{ case c: ClassLike => c.name },
+    classNames <<= classNames storeAs classNames triggeredBy (compile in Compile),
     fileName := "classDiagram.svg",
     classDiagram <<= writeTask.map{ svg =>
       java.awt.Desktop.getDesktop.open(svg)
@@ -32,5 +51,14 @@ object Plugin extends sbt.Plugin {
     classDiagramWrite <<= writeTask
   )
 
+  private def createParser(classNames: Seq[String]): Parser[Seq[String]] = {
+    classNames match {
+      case Nil =>
+        defaultParser
+      case _   =>
+        val other = Space ~> token(StringBasic, _ => true)
+        ((Space ~> classNames.distinct.map(token(_)).reduce(_ | _)) | other).*
+    }
+  }
 }
 
