baf1664265ffe77f59e8ad8d5c19e2cd7f95f64a
xuwei-k <6b656e6a69@gmail.com>
2024-10-07 20:41:50 +0900

    update Serialization.scala

--- src/main/scala-2.12/Serialization.scala
+++ src/main/scala-2.12/Serialization.scala
@@ -1,6 +1,6 @@
 package diagram
 
-@deprecated(message = "will be removed", since = "0.2.2")
+@deprecated(message = "will be removed", since = "0.3.0")
 private[diagram] object Serialization {
   val Implicits = sjsonnew.BasicJsonProtocol
 }
