9714d030697714918ef5cad78feb4f526b2ae7ae
kenji yoshida <6b656e6a69@gmail.com>
2024-10-07 19:02:17 +0900

    delete scala-sbt-0.13/Serialization.scala

--- src/main/scala-sbt-0.13/Serialization.scala
+++
@@ -1,7 +0,0 @@
-package diagram
-
-private[diagram] object Serialization {
-  object Implicits extends sbinary.DefaultProtocol {
-    implicit def seqFormat[A: sbinary.Format] = sbt.Cache.seqFormat[A]
-  }
-}
